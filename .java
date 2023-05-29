27f7ef28ef08f762974647f514e696a4efa285e6
Rahul <rahulrakh21@gmail.com>
2023-05-29 18:12:17 +0000

    latest update

---
+++ .java
@@ -0,0 +1,11 @@
+//write a program to calculate sum of two numbers
+class add
+{
+	public static void main(String args[])
+	{
+		int a=10, b=29;
+		int sum=a+b;
+		System.out.println("Addition is ="+sum);
+	}
+}
+
